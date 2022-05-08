f78a4fb4742ee3883e41fbadcac40199cb32a376
Sherkhon <issakvell@gmail.com>
2022-05-08 12:54:49 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var elBoxIn = document.querySelector(".box-in")
+var elBoxContent = document.querySelector(".box-content")
+elBoxIn.addEventListener("click", function() {
+  elBoxContent.classList.toggle("box-content--open")
+} )
+
